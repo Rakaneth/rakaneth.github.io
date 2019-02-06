797cda52210343f2d290fea3d6a7318514baf77e
Rakaneth <rakaneth@gmail.com>
2019-02-06 14:59:33 -0500

    UI cleanup

---
+++ app.js
@@ -0,0 +1,12 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        items: RIPDATA,
+        knacks: KNACKS,
+        selected: {},
+        chosenKnacks: [],
+        loadouts: [],
+        curLoadout: []
+    }
+})
+
